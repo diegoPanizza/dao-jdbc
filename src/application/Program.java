d95f88748494d89eb2d05d9ee32f4ad2117b1f82
Diego Panizza <diego_panizza@hotmail.com>
2023-06-29 11:32:33 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -29,6 +30,10 @@ public class Program {
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
-
+		
+		System.out.println("===\nTEST 4: Seller insert===");
+		Seller newSeller = new Seller(null, "Diego", "diego@gmail.com", new Date(), 5000.0, department);
+		sellerDao.insert(newSeller); 
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 }
